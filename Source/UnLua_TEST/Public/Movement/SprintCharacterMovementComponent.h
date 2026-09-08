3ea135a912ac9afc6a02f178ddde8d406c2f41a0
YINGYUE01 <154315235+YINGYUE01@users.noreply.github.com>
2026-09-08 22:21:48 +0800

    Fix sprint movement component UE 5.2 build

---
+++ Source/UnLua_TEST/Public/Movement/SprintCharacterMovementComponent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SprintCharacterMovementComponent.generated.h"
+
+class FSavedMove_Sprint;
+
+UCLASS()
+class UNLUA_TEST_API USprintCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	USprintCharacterMovementComponent();
+
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (ClampMin = "0.0"))
+	float SprintSpeed = 600.f;
+
+	void SetWantsToSprint(bool bNewWantsToSprint);
+
+	virtual float GetMaxSpeed() const override;
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+private:
+	friend class FSavedMove_Sprint;
+
+	uint8 bWantsToSprint : 1;
+};
